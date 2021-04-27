e198145cae8f83659f8fc36b3b471ba7fdc0ad9b
bit-user <bit-user@DESKTOP-C1QR4BV>
2021-04-27 17:12:49 +0900

    prob05 solved #1

--- practice02/src/main/java/prob05/Prob05.java
+++ practice02/src/main/java/prob05/Prob05.java
@@ -9,15 +9,47 @@ public class Prob05 {
 		Scanner scanner = new Scanner( System.in );
 
 		while( true ) {
-			
+
 			/* 게임 작성 */
 
 			// 정답 램덤하게 만들기
 			Random random = new Random();
 			int correctNumber = random.nextInt( 100 ) + 1;
-			System.out.println(correctNumber);
+			System.out.println("수를 결정하였습니다. 맞추어 보세요.");
+			System.out.println("1-100");
+			boolean correct = false;
 
+			int cnt = 1;
+			int min = 0;
+			int max = 100;
 			
+			while (!correct) {
+				System.out.print(cnt + ">>");
+				int num = scanner.nextInt();
+
+				cnt++;
+
+				if (num == correctNumber) {
+					System.out.println("맞았습니다.");
+					correct = true;
+				} else if (num < correctNumber) {
+					System.out.println("더 높게");
+					if (num > min) {
+						min = num;
+					}
+					System.out.println(min +"-" + max);
+				} else {
+					System.out.println("더 낮게");
+					if (num < max) {
+						max = num;
+					}
+					System.out.println(min + "-" + max);
+				}
+			}
+
+			//System.out.println(correctNumber);
+
+
 			//새 게임 여부 확인하기
 			System.out.print( "다시 하겠습니까(y/n)>>" );
 			String answer = scanner.next();
@@ -25,7 +57,7 @@ public class Prob05 {
 				break;
 			}
 		}
-		
+
 		scanner.close();
 	}
 
