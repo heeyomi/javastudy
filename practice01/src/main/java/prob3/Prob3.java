b2d7d3d2c70ea2bb6691bf7ac7a994dec93cd743
bit-user <bit-user@DESKTOP-C1QR4BV>
2021-04-27 15:50:07 +0900

    prob3 solved #1

--- practice01/src/main/java/prob3/Prob3.java
+++ practice01/src/main/java/prob3/Prob3.java
@@ -9,6 +9,24 @@ public class Prob3 {
 		Scanner scanner = new Scanner(System.in);
 
 		/* 코드 작성 */
+		System.out.println("숫자를 입력하세요 :");
+		int num = scanner.nextInt();
+		int sum = 0;
+		
+		if (num % 2 == 0) {
+			for (int i = 0; i <= num; i++) {
+				if (i%2 == 0) {
+					sum += i;
+				}
+			}
+		} else {
+			for (int i = 0; i <= num; i++) {
+				if (i%2 != 0) {
+					sum += i;
+				}
+			}
+		}
+		System.out.println("결과값 : " + sum);
 		
 		scanner.close();
 	}
