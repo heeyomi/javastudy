7be7b2ce2da50fb3edfc0ef4c51c2cfd3a88fedb
bit-user <bit-user@DESKTOP-C1QR4BV>
2021-04-28 16:30:10 +0900

    prob04 solved #3

--- practice03/src/main/java/prob04/StringUtil.java
+++ practice03/src/main/java/prob04/StringUtil.java
@@ -1,4 +1,13 @@
 package prob04;
 
 public class StringUtil {
+	public static String concatenate(String[] str) {
+		String temp = "";
+		
+		for (int i = 0; i < str.length; i++) {
+			temp += str[i];
+		}
+		
+		return temp;
+	}
 }
