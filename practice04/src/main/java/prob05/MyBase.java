352ce5ead727d954239c4bf0831e27ef116da8a8
bit-user <bit-user@DESKTOP-C1QR4BV>
2021-04-29 16:47:49 +0900

    prob05 solved #4

---
+++ practice04/src/main/java/prob05/MyBase.java
@@ -0,0 +1,17 @@
+package prob05;
+
+public class MyBase extends Base{
+	@Override
+	public void service(String state) {
+		if (state.equals("오후")) {
+			System.out.println("오후도 낮과 마찬가지로 일해야 합니다.");
+		} else {
+			super.service(state);
+		}
+	}
+	
+	@Override
+	public void day() {
+		System.out.println("낮에는 열심히 일하자!");
+	}
+}
