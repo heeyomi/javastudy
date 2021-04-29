ae84732da5e72ea7815b770ae88c8ebac095b6dc
bit-user <bit-user@DESKTOP-C1QR4BV>
2021-04-29 17:40:52 +0900

    prob03 solved #5

---
+++ practice05/src/main/java/prob3/Duck.java
@@ -0,0 +1,28 @@
+package prob3;
+
+public class Duck extends Bird {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void fly() {
+		System.out.println("오리(" + getName() + ")는 날지 않습니다.");
+	}
+	
+	@Override
+	public void sing() {
+		System.out.println("오리(" + getName() + ")가 소리내어 웁니다.");
+	}
+	
+	@Override
+	public String toString() {
+		return "오리의 이름은 " + this.name + "입니다.";
+	}
+}
