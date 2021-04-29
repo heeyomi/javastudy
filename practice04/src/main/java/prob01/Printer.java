1779ed86620a5687f5a833c7ecd50e93d930674f
bit-user <bit-user@DESKTOP-C1QR4BV>
2021-04-29 16:55:37 +0900

    prob01 solved #4

---
+++ practice04/src/main/java/prob01/Printer.java
@@ -0,0 +1,21 @@
+package prob01;
+
+public class Printer {
+
+	public void println(int num) {
+		System.out.println(num);
+	}
+
+	public void println(String message) {
+		System.out.println(message);
+	}
+
+	public void println(Double num) {
+		System.out.println(num);
+	}
+
+	public void println(boolean flag) {
+		System.out.println(flag);
+	}
+
+}
