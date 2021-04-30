b7a935a070537f68b2ef16c7b34c1394ee2eb9e6
bit-user <bit-user@DESKTOP-C1QR4BV>
2021-04-30 16:46:56 +0900

    prob02 solved #5

--- practice05/src/main/java/prob2/SmartPhone.java
+++ practice05/src/main/java/prob2/SmartPhone.java
@@ -5,11 +5,19 @@ public class SmartPhone extends MusicPhone {
 	@Override
 	public void execute(String function) {
 		if (function.equals("음악")) {
-			System.out.println("다운로드해서 음악재생");
+			playMusic();
 		} else if (function.equals("통화")) {
 			super.execute("통화");
 		} else if (function.equals("앱")) {
-			System.out.println("앱 실행");
+			playApp();
 		}
+	}
+	
+	private void playMusic() {
+		System.out.println("다운로드해서 음악재생");
+	}
+	
+	private void playApp() {
+		System.out.println("앱 실행");
 	}
 }
