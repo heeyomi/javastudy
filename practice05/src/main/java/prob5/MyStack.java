54c0cee513b24c5c8adc5e315ff00984e1a8075c
bit-user <bit-user@DESKTOP-C1QR4BV>
2021-04-30 17:40:07 +0900

    prob05 solved #5

--- practice05/src/main/java/prob5/MyStack.java
+++ practice05/src/main/java/prob5/MyStack.java
@@ -1,4 +1,45 @@
 package prob5;
 
 public class MyStack {
+
+	private String[] stack;
+	private int top = -1;
+	
+	public MyStack(int size) {
+		stack = new String[size];
+	}
+
+	public void push(String string) {
+		if (stack.length -1 == top) {
+			resize();
+		}
+		
+		stack[++top] = string;
+	}
+	
+	public void resize() {
+		String[] temp = new String[stack.length * 2];
+		
+		for (int i = 0; i < top; i++) {
+			temp[i] = stack[i];
+		}
+		
+		stack = temp;
+	}
+
+	public boolean isEmpty() {
+		return top == -1;
+	}
+
+	public String pop() throws MyStackException{
+		if (isEmpty()) {
+			throw new MyStackException("Stack is Empty");
+		}
+		
+		String result = stack[top];
+		stack[top--] = null;
+		
+		return result;
+	}
+	
 }
---
+++ practice05/src/main/java/prob5/MyStackException.java
@@ -0,0 +1,11 @@
+package prob5;
+
+public class MyStackException extends NullPointerException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public MyStackException(String message) {
+		super(message);
+	}
+
+}
