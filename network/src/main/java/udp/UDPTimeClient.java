42a5f88b59e1f391f8cd42928b691f4c82af3736
yoonk <yoonk@heeyomi>
2021-05-07 13:43:50 +0900

    UDP Time C/S

--- network/src/main/java/udp/UDPTimeClient.java
+++ network/src/main/java/udp/UDPTimeClient.java
@@ -1,9 +1,48 @@
 package udp;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+
 public class UDPTimeClient {
+	private static final String SERVER_IP = "127.0.0.1";
+	private static final int SERVER_PORT = UDPEchoServer.PORT;
+	public static final int BUFFER_SIZE= UDPEchoServer.BUFFER_SIZE;
 
 	public static void main(String[] args) {
 
+		DatagramSocket socket = null;
+
+		try {
+			// 2. socket 생성
+			socket =  new DatagramSocket();
+
+			while (true) {
+
+				// 4. 데이터 쓰기
+				byte[] sendData = "".getBytes("UTF-8");
+				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, new InetSocketAddress(SERVER_IP, SERVER_PORT));
+				socket.send(sendPacket);
+
+				// 5. 데이터 읽기
+				DatagramPacket receivePacket = new DatagramPacket(new byte[BUFFER_SIZE], BUFFER_SIZE);
+				socket.receive(receivePacket); //blocking
+
+				byte[] receiveData = receivePacket.getData();
+				int length = receivePacket.getLength();
+				String now = new String(receiveData, 0, length, "UTF-8");
+
+				System.out.println(now);
+			}
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (socket != null && socket.isClosed() == false) {
+				socket.close();
+			}
+		}
 	}
 
 }
--- network/src/main/java/udp/UDPTimeServer.java
+++ network/src/main/java/udp/UDPTimeServer.java
@@ -1,22 +1,41 @@
 package udp;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import javax.xml.crypto.Data;
 
 public class UDPTimeServer {
 
-	public static void main(String[] args) {
-		// 2. 데이터 수신
-		DatagramPacket receivePacket = new DatagramPacket(new byte[BUFFER_SIZE], BUFFER_SIZE);
-		socket.receive(receivePacket); //blocking
-		
-		// 3. 데이터 송신
-		String now = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date());
-		byte[] sendData = "".getBytes();
-		
-	}
+	public static final int PORT = 7000;
+	public static final int BUFFER_SIZE = 1024;
 
+	public static void main(String[] args) {
+		DatagramSocket socket = null;
+
+		try {
+			// 1. 소켓 생성
+			socket = new DatagramSocket(PORT);
+			
+			while (true) {
+				// 2. 데이터 수신
+				DatagramPacket receivePacket = new DatagramPacket(new byte[BUFFER_SIZE], BUFFER_SIZE);
+				socket.receive(receivePacket); //blocking
+				
+				// 3. 데이터 송신
+				String now = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date());
+				byte[] sendData = now.getBytes();
+				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(), receivePacket.getPort());
+				socket.send(sendPacket);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (socket!=null && !socket.isClosed()) {
+				socket.isClosed();
+			}
+		}
+	}
 }
